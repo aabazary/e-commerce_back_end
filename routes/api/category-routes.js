28ceb584d33dce1bd2e1cd33243ec7843354ec11
aabazary <aabazary@gmail.com>
2021-09-06 03:46:42 -0700

    added global get routes for each route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,16 +1,51 @@
 const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const {
+  Category,
+  Product
+} = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categoryDB = await Category.findAll({
+      include: [{
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }]
+    })
+    res.status(200).json(categoryDB);
+  } catch (err) {
+    res.status(500).json(err);
+  };
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryDB = await Category.findOne({
+        where: {
+          id: req.params.id
+        },
+        include: [{
+          model: Product,
+          attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+        }]
+      });
+      if (!categoryDB) {
+        res.status(404).json({
+          message: 'No Category found with that id!'
+        });
+        return;
+      }
+  
+      res.status(200).json(categoryDB);
+    } catch (err) {
+      res.status(500).json(err);
+    }
 });
 
 router.post('/', (req, res) => {
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,16 +1,52 @@
 const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const {
+  Tag,
+  Product,
+  ProductTag
+} = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try{
+    const tagDB = await Tag.findAll({
+      include: [{
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+      }]
+    })
+  res.status(200).json(tagDB);
+  } catch(err) {
+      res.status(500).json(err);
+    };
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagDB = await Tag.findOne({
+        where: {
+          id: req.params.id
+        },
+        include: [{
+          model: Product,
+          attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+        }]
+      });
+      if (!tagDB) {
+        res.status(404).json({
+          message: 'No Tag found with that id!'
+        });
+        return;
+      }
+  
+      res.status(200).json(tagDB);
+    } catch (err) {
+      res.status(500).json(err);
+    }
 });
 
 router.post('/', (req, res) => {
