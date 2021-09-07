715e6d9b3906d8310809975a1f182d657806b2da
aabazary <aabazary@gmail.com>
2021-09-06 17:01:39 -0700

    corrected products routes, functional code

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -149,8 +149,26 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productDB = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!productDB) {
+      res.status(404).json({
+        message: 'No Product found with this id!'
+      });
+      return;
+    }
+
+    res.status(200).json(productDB);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
